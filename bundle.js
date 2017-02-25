eeed5e563466a9481035dcfebe304695fc6c869f
Leo Salat <leo.salat@gmail.com>
2017-02-25 10:06:00 -0800

    refactor checkCollision() in game module

--- bundle.js
+++ bundle.js
@@ -11,16 +11,16 @@
 
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -33,243 +33,268 @@
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-	const GameView = __webpack_require__(1);
+function MovingObject(pos, vel, rad, col, game) {
+  this.pos = pos;
+  this.vel = vel;
+  this.rad = rad;
+  this.col = col;
+  this.game = game;
+}
 
-	document.addEventListener("DOMContentLoaded", function(event) {
-	    const ctx = document.getElementById('game-canvas').getContext('2d');
-	    new GameView().start(ctx);
-	});
+
+MovingObject.prototype.draw = function(ctx) {
+  ctx.fillStyle = this.col;
+  ctx.beginPath();
+
+  ctx.arc(
+    this.pos[0],
+    this.pos[1],
+    this.rad,
+    0,
+    2 * Math.PI
+  );
+
+  ctx.fill();
+};
+
+MovingObject.prototype.move = function () {
+  this.pos[0] += this.vel[0];
+  this.pos[1] += this.vel[1];
+  this.pos = this.game.wrap(this.pos);
+};
+
+MovingObject.prototype.isCollidedWith = function(that) {
+  const dist = Math.sqrt(Math.pow((this.pos[0] - that.pos[0]), 2) + Math.pow((this.pos[1] - that.pos[1]), 2));
+  return dist < (this.rad + that.rad);
+};
+
+MovingObject.prototype.collideWith = function(that) {
+  // this.game.remove(this);
+  // this.game.remove(that);
+};
+
+module.exports = MovingObject;
 
 
-/***/ },
+/***/ }),
 /* 1 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	const Game = __webpack_require__(2);
+const MovingObject = __webpack_require__(0);
+const Util = __webpack_require__(2);
 
-	function GameView(ctx) {
-	  this.game = new Game();
-	  this.ctx = ctx;
-	}
+function Ship(game) {
+  this.game = game;
+  MovingObject.call(this, game.randomPosition(), [0,0], 20, 'blue', game);
+}
 
-	GameView.prototype.start = function(ctx) {
-	  setInterval(() => {
-	    this.game.draw(ctx);
-	    this.game.step();
-	  }, 20);
-	};
+Util.inherits(Ship, MovingObject);
 
-	module.exports = GameView;
+Ship.prototype.relocate = function () {
+  this.vel = [0,0];
+  this.pos = this.game.randomPosition();
+};
+module.exports = Ship;
 
 
-/***/ },
+/***/ }),
 /* 2 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-	const Asteroid = __webpack_require__(3);
-	const Ship = __webpack_require__(6);
+const Util = {
+  inherits (childClass, parentClass) {
+    function Surrogate() {}
+    Surrogate.prototype = parentClass.prototype;
+    childClass.prototype = new Surrogate();
+    childClass.prototype.constructor = childClass;
+  },
 
-	function Game() {
-	  this.DIM_X = 800;
-	  this.DIM_Y = 800;
-	  this.NUM_ASTEROIDS = 8;
-	  this.asteroids = [];
-	  this.addAsteroids();
-	  this.ship = new Ship(this);
-	}
-
-	Game.prototype.randomPosition = function() {
-	  return [Math.random() * 800, Math.random() * 800];
-	};
-
-	Game.prototype.addAsteroids = function() {
-	  for (let i = 0; i < this.NUM_ASTEROIDS; i++) {
-	    let newAstr = new Asteroid(this.randomPosition(), this);
-	    this.asteroids.push(newAstr);
-	  }
-	};
-
-	Game.prototype.draw = function(ctx) {
-	  ctx.clearRect(0, 0, 800, 800);
-	  let all = this.allObjects();
-	  all.forEach((obj) => obj.draw(ctx));
-	};
-
-	Game.prototype.moveObjects = function() {
-	  this.allObjects().forEach((obj) => obj.move());
-	};
-
-	Game.prototype.wrap = function(pos) {
-	  if (pos[0] > this.DIM_X) {
-	    pos[0] = 0;
-	  }else if (pos[0] < 0) {
-	    pos[0] = this.DIM_X;
-	  }
-	  if (pos[1] > this.DIM_Y) {
-	    pos[1] = 0;
-	  }else if (pos[1] < 0) {
-	    pos[1] = this.DIM_Y;
-	  }
-	  return pos;
-	};
-
-	Game.prototype.checkCollisions = function() {
-	  this.allObjects().forEach((obj1, idx) => {
-	    this.allObjects().slice(idx + 1).forEach((obj2) => {
-	    // this.allObjects().forEach((obj2) => {
-	      if (obj1.isCollidedWith(obj2)) {
-	        obj1.collideWith(obj2);
-	        obj2.collideWith(obj1);
-	      }
-	    });
-	  });
-	};
-
-	Game.prototype.step = function() {
-	  this.checkCollisions();
-	  this.moveObjects();
-	};
-
-	Game.prototype.remove = function(asteroid) {
-	  let i = this.asteroids.indexOf(asteroid);
-	  this.asteroids.splice(i, 1);
-	};
-
-	Game.prototype.allObjects = function() {
-	  return this.asteroids.concat(this.ship);
-	};
-
-	module.exports = Game;
+  randomVec (length) {
+    const deg = 2 * Math.PI * Math.random();
+    return Util.scale([Math.sin(deg), Math.cos(deg)], length);
+  },
+  // Scale the length of a vector by the given amount.
+  scale (vec, m) {
+    return [vec[0] * m, vec[1] * m];
+  }
+};
 
 
-/***/ },
+module.exports = Util;
+
+
+/***/ }),
 /* 3 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	const MovingObject = __webpack_require__(4);
-	const Util = __webpack_require__(5);
-	const Ship = __webpack_require__(6);
+const Game = __webpack_require__(5);
 
-	function Asteroid(pos, game) {
-	  this.vel = Util.randomVec(5);
-	  MovingObject.call(this, pos, this.vel, 40, 'grey', game);
-	}
+function GameView(ctx) {
+  this.game = new Game();
+  this.ctx = ctx;
+}
 
-	Asteroid.prototype.collideWith = function(otherObject) {
-	  if (otherObject instanceof Ship) {
-	    otherObject.relocate();
-	  }
-	};
+GameView.prototype.start = function(ctx) {
+  setInterval(() => {
+    this.game.draw(ctx);
+    this.game.step();
+  }, 20);
+};
 
-	Util.inherits(Asteroid, MovingObject);
-	module.exports = Asteroid;
+module.exports = GameView;
 
 
-/***/ },
+/***/ }),
 /* 4 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	function MovingObject(pos, vel, rad, col, game) {
-	  this.pos = pos;
-	  this.vel = vel;
-	  this.rad = rad;
-	  this.col = col;
-	  this.game = game;
-	}
+const MovingObject = __webpack_require__(0);
+const Util = __webpack_require__(2);
+const Ship = __webpack_require__(1);
 
+function Asteroid(pos, game) {
+  this.vel = Util.randomVec(5);
+  MovingObject.call(this, pos, this.vel, 40, 'grey', game);
+}
 
-	MovingObject.prototype.draw = function(ctx) {
-	  ctx.fillStyle = this.col;
-	  ctx.beginPath();
+Asteroid.prototype.collideWith = function(otherObject) {
+  if (otherObject instanceof Ship) {
+    otherObject.relocate();
+  }
+};
 
-	  ctx.arc(
-	    this.pos[0],
-	    this.pos[1],
-	    this.rad,
-	    0,
-	    2 * Math.PI
-	  );
-
-	  ctx.fill();
-	};
-
-	MovingObject.prototype.move = function () {
-	  this.pos[0] += this.vel[0];
-	  this.pos[1] += this.vel[1];
-	  this.pos = this.game.wrap(this.pos);
-	};
-
-	MovingObject.prototype.isCollidedWith = function(that) {
-	  // console.log('check');
-	  const dist = Math.sqrt(Math.pow((this.pos[0] - that.pos[0]), 2) + Math.pow((this.pos[1] - that.pos[1]), 2));
-	  return dist < (this.rad + that.rad);
-	};
-
-	MovingObject.prototype.collideWith = function(that) {
-	  // this.game.remove(this);
-	  // this.game.remove(that);
-	};
-
-	module.exports = MovingObject;
+Util.inherits(Asteroid, MovingObject);
+module.exports = Asteroid;
 
 
-/***/ },
+/***/ }),
 /* 5 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	const Util = {
-	  inherits (childClass, parentClass) {
-	    function Surrogate() {}
-	    Surrogate.prototype = parentClass.prototype;
-	    childClass.prototype = new Surrogate();
-	    childClass.prototype.constructor = childClass;
-	  },
+const Asteroid = __webpack_require__(4);
+const Ship = __webpack_require__(1);
 
-	  randomVec (length) {
-	    const deg = 2 * Math.PI * Math.random();
-	    return Util.scale([Math.sin(deg), Math.cos(deg)], length);
-	  },
-	  // Scale the length of a vector by the given amount.
-	  scale (vec, m) {
-	    return [vec[0] * m, vec[1] * m];
-	  }
-	};
+function Game() {
+  this.DIM_X = 800;
+  this.DIM_Y = 800;
+  this.NUM_ASTEROIDS = 6;
+  this.asteroids = [];
+  this.addAsteroids();
+  this.ship = new Ship(this);
+}
+
+Game.prototype.randomPosition = function() {
+  return [Math.random() * 800, Math.random() * 800];
+};
+
+Game.prototype.addAsteroids = function() {
+  for (let i = 0; i < this.NUM_ASTEROIDS; i++) {
+    let newAstr = new Asteroid(this.randomPosition(), this);
+    this.asteroids.push(newAstr);
+  }
+};
+
+Game.prototype.draw = function(ctx) {
+  ctx.clearRect(0, 0, 800, 800);
+  let all = this.allObjects();
+  all.forEach(obj => obj.draw(ctx));
+};
+
+Game.prototype.moveObjects = function() {
+  this.allObjects().forEach(obj => obj.move());
+};
+
+Game.prototype.wrap = function(pos) {
+  if (pos[0] > this.DIM_X) {
+    pos[0] = 0;
+  }else if (pos[0] < 0) {
+    pos[0] = this.DIM_X;
+  }
+  if (pos[1] > this.DIM_Y) {
+    pos[1] = 0;
+  }else if (pos[1] < 0) {
+    pos[1] = this.DIM_Y;
+  }
+  return pos;
+};
+
+Game.prototype.checkCollisions = function() {
+  this.allObjects().forEach((obj1, idx1) => {
+    // this.allObjects().slice(idx + 1).forEach((obj2) => {
+    this.allObjects().forEach((obj2, idx2) => {
+      if (idx1 === idx2) return false;
+      if (obj1.isCollidedWith(obj2)) {
+        obj1.collideWith(obj2);
+      }
+    });
+  });
+};
+
+Game.prototype.step = function() {
+  this.checkCollisions();
+  this.moveObjects();
+};
+
+Game.prototype.remove = function(asteroid) {
+  let i = this.asteroids.indexOf(asteroid);
+  this.asteroids.splice(i, 1);
+};
+
+Game.prototype.allObjects = function() {
+  return this.asteroids.concat(this.ship);
+};
+
+module.exports = Game;
 
 
-	module.exports = Util;
-
-
-/***/ },
+/***/ }),
 /* 6 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	const MovingObject = __webpack_require__(4);
-	const Util = __webpack_require__(5);
+const GameView = __webpack_require__(3);
 
-	function Ship(game) {
-	  this.game = game;
-	  MovingObject.call(this, game.randomPosition(), [0,0], 40, 'blue', game);
-	}
-
-	Util.inherits(Ship, MovingObject);
-
-	Ship.prototype.relocate = function () {
-	  this.vel = [0,0];
-	  this.pos = this.game.randomPosition();
-	};
-	module.exports = Ship;
+document.addEventListener("DOMContentLoaded", function(event) {
+    const ctx = document.getElementById('game-canvas').getContext('2d');
+    new GameView().start(ctx);
+});
 
 
-/***/ }
+/***/ })
 /******/ ]);
